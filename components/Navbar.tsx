fa8382deeb333b9351a1e371a67b16e3eef4a1bf
phaaz <109876495+phaaz@users.noreply.github.com>
2022-07-23 18:53:54 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -56,7 +56,71 @@ const Navbar: FC = () => {
   useEffect(() => {
     setShowLinks(externalLinks.length > 0)
   }, [])
-  
+
+  useEffect(() => {
+    if (filterableCollection) {
+      const href = getInitialSearchHref()
+
+      fetch(href).then(async (res) => {
+        let initialResults = undefined
+
+        if (res.ok) {
+          initialResults =
+            (await res.json()) as paths['/search/collections/v1']['get']['responses']['200']['schema']
+        }
+
+        const smallCommunity =
+          initialResults?.collections &&
+          initialResults.collections.length >= 2 &&
+          initialResults.collections.length <= 10
+
+        if (
+          !DEFAULT_TO_SEARCH &&
+          (COMMUNITY || COLLECTION_SET_ID) &&
+          smallCommunity
+        ) {
+          setFilterComponent(
+            <CommunityDropdown
+              collections={initialResults?.collections}
+              defaultCollectionId={COLLECTION}
+            />
+          )
+        } else {
+          setShowLinks(false)
+          setFilterComponent(
+            <SearchCollections
+              communityId={COMMUNITY}
+              initialResults={initialResults}
+            />
+          )
+        }
+      })
+    }
+  }, [filterableCollection])
+
+  return (
+    <nav className="relative col-span-full flex items-center justify-between gap-2 px-6 py-4 md:gap-3 md:py-6 md:px-16">
+      <NavbarLogo className="z-10 max-w-[300px]" />
+      {showLinks && (
+        <div className="z-10 ml-12 hidden items-center gap-11 lg:flex">
+          {externalLinks.map(({ name, url }) => (
+            <a
+              key={url}
+              href={url}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-dark reservoir-h6 hover:text-[#1F2937] dark:text-white"
+            >
+              {name}
+            </a>
+          ))}
+        </div>
+      )}
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="absolute left-0 z-[1] flex w-full justify-center">
+          {filterComponent && filterComponent}
+        </div>
+      </div>
       <HamburgerMenu externalLinks={externalLinks} />
       <div className="z-10 ml-auto hidden shrink-0 md:flex md:gap-2">
         <ConnectWallet />
